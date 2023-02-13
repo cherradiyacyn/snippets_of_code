f0ddd4bb23e0d34c7e6a5fbe0014dc4e7a6a7ca8
Yacyn CHERRADI <cherradiyacyn@gmail.com>
2023-02-13 12:11:13 +0100

    getPath in `windows style` else Linux' is default

--- getExecutablePath.go
+++ getExecutablePath.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 	"strings"
 )
 
 func main() {
 	file, _ := os.Executable()
-	index := strings.LastIndexByte(file, '\\')
+	var index int
+	if runtime.GOOS == "windows" {
+		index = strings.LastIndexByte(file, '\\')
+	} else {
+		index = strings.LastIndexByte(file, '/')
+	}
 	pathname := file[:index+1]
 	fmt.Printf("file: %s\npath: %s\n", file, pathname)
 }
